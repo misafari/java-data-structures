a8f4112e15615dab6af9df7995513a26f21370e4
ms.safari <ms.safari@outlook.com>
2023-02-10 21:10:45 +0300

    cleaning the code and implementation of kth

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,9 @@
 
 import implementation.array.linkedList.SinglyLinkedList;
 
-import java.util.LinkedList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         SinglyLinkedList<Integer> a = new SinglyLinkedList<>();
-        List<Integer> b = new LinkedList<>();
 
         a.add(10);
         a.add(20);
@@ -15,8 +11,6 @@ public class Main {
         a.add(40);
         a.add(50);
 
-        a.reverse();
-
-        a.print();
+        System.out.println(a.getKthFromTheEnd(2));
     }
 }
--- src/implementation/array/linkedList/Node.java
+++ src/implementation/array/linkedList/Node.java
@@ -1,31 +1,19 @@
 package implementation.array.linkedList;
 
-public class Node<T> {
-    private final T value;
-    private Node<T> next;
+class Node<T> {
+    final T value;
+    Node<T> next;
 
-    public Node(T value) {
+    Node(T value) {
         this(value, null);
     }
 
-    public Node(T value, Node<T> next) {
+    Node(T value, Node<T> next) {
         this.value = value;
         this.next = next;
     }
 
-    public boolean contain(T value) {
+    boolean contain(T value) {
         return this.value == value;
-    }
-
-    public T getValue() {
-        return value;
-    }
-
-    public Node<T> getNext() {
-        return next;
-    }
-
-    public void setNext(Node<T> next) {
-        this.next = next;
     }
 }
